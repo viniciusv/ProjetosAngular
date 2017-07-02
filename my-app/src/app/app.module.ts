872d11ecc45b7db9ae6d8168a9c78d737fbbadf0
Vinicius Viana <viniciussantosviana@gmail.com>
2017-07-02 15:59:59 -0300

    Primeiro commit - Curso angular 2 - Aprendendo sobre module

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MeuPirmeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+import { CursosModule } from "./cursos/cursos.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPirmeiroComponent,
+    MeuPrimeiro2Component
+  ],
+  imports: [
+    BrowserModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <h1>Primeiro  component</h1>
+    `
+})
+export class MeuPirmeiroComponent {}
