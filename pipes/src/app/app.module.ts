9ae53e761644875a193349a0a239cce1ef88916c
Vinicius Viana <viniciussantosviana@gmail.com>
2017-07-12 18:59:31 -0300

    pipes e services

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe,
+    FiltroArrayPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [ 
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: settingsService => settingsService.getLocale()
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
